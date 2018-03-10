7f799793b10a70ed625f5d36bd1f076bb23010fe
Ismail Ghallou <ismai23l@hotmail.com>
2018-03-10 09:21:15 +0000

    Done coding on train on mobile

--- src/components/Header/Sidebar/index.jsx
+++ src/components/Header/Sidebar/index.jsx
@@ -3,8 +3,8 @@ import Link from 'gatsby-link'
 
 import './style.scss'
 
-const Sidebar = ({sidebar}) => (
-    <div className={`sidebar ${sidebar}`}>
+const Sidebar = ({sidebar, toggle}) => (
+    <div className={`sidebar ${sidebar}`} onClick={toggle} >
         <div className="items">
         <Link to="/" activeClassName="current" exact={true} >Home</Link>
         <Link to="/about" activeClassName="current">About</Link>
