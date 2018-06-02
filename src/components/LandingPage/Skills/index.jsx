6e7864c46ae9a57c8bffce031086833cd68bc62b
smakosh <ismai23l@hotmail.com>
2018-06-02 23:27:36 +0000

    refactoring code while travelling

--- src/components/LandingPage/Skills/index.jsx
+++ src/components/LandingPage/Skills/index.jsx
@@ -2,49 +2,36 @@ import React from 'react'
 
 import { Container } from '../../common'
 import Skill from './Skill'
-
 import Graphic from '../../../../static/skills/graphic.svg'
 import Uix from '../../../../static/skills/ui.svg'
 import WebDev from '../../../../static/skills/webdev.svg'
-
 import './style.scss'
 
-const Skills = ({firstImg, secondImg, thirdImg}) => (
-    <div
-        style={{
-            marginBottom: '1.45rem',
-            color: 'white',
-            padding: '8rem 1rem 4rem 1rem'
-        }}
-        className="clip gradient-blue"
-    >   
-        <Container>
-            <h2>
-                Skills
-            </h2>
-            <div
-                style={{
-                    margin: '0 auto',
-                    maxWidth: 960,
-                    padding: '2rem 0'
-                }}
-                className="skill-container"
-            >
-                <Skill
-                    title="Graphic Design"
-                    icon={Graphic}
-                />
-                <Skill
-                    title="UI/UX Design"
-                    icon={Uix}
-                />
-                <Skill
-                    title="Front end development"
-                    icon={WebDev}
-                />
-            </div>
-        </Container>
-    </div>
-)
+const Skills = ({firstImg, secondImg, thirdImg}) => {
+    const services = [
+        {
+            title: 'Graphic Design',
+            icon: `${Graphic}`
+        },
+        {
+            title: 'UI/UX Design',
+            icon: `${Uix}`
+        },
+        {
+            title: 'Front end development',
+            icon: `${WebDev}`
+        },
+    ]
+    return (
+        <div className="clip gradient-blue services">   
+            <Container>
+                <h2>Skills</h2>
+                <div className="skill-container">
+                    {services.map((service, index) => <Skill {...service} />)}
+                </div>
+            </Container>
+        </div>
+    )
+}
 
 export { Skills }
