a187bf4599d1b412333638d5c61216ade3b7a628
smakosh <ismai23l@hotmail.com>
2018-12-07 14:53:43 +0100

    new portrait picture

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -56,12 +56,10 @@ const Row = styled.div`
 	justify-content: space-between;
 	align-items: center;
 	flex-wrap: wrap;
-
 	&:after {
 		content: "";
 		max-width: 32%;
 		width: 100%;
-
 		@media (max-width: 960px) {
 			content: unset;
 		}
