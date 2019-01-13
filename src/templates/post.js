9508fc25c602ba7727acf6b7ac82d94adae36fb9
smakosh <ismai23l@hotmail.com>
2019-01-13 21:02:23 +0100

    article/2018-year-review

--- src/templates/post.js
+++ src/templates/post.js
@@ -4,46 +4,46 @@ import { Layout, SmallerContainer, SEO, Post } from 'Common'
 import './highlight.scss'
 
 const Template = ({ data: { post } }) => (
-  <Layout>
-    <SmallerContainer>
-      <SEO
-        type="NewsArticle"
-        title={post.frontmatter.title}
-        articleBody={post.html}
-        datePublished={post.frontmatter.date}
-        dateModified={
-          post.frontmatter.edited
-            ? post.frontmatter.edited
-            : post.frontmatter.date
-        }
-        cover={post.frontmatter.thumbnail.childImageSharp.fluid.originalImg}
-        location={post.frontmatter.path}
-      />
-      <Post {...post} />
-    </SmallerContainer>
-  </Layout>
+	<Layout>
+		<SmallerContainer>
+			<SEO
+				type="NewsArticle"
+				title={post.frontmatter.title}
+				articleBody={post.html}
+				datePublished={post.frontmatter.date}
+				dateModified={
+					post.frontmatter.edited
+						? post.frontmatter.edited
+						: post.frontmatter.date
+				}
+				cover={post.frontmatter.thumbnail.childImageSharp.fluid.originalImg}
+				location={post.frontmatter.path}
+			/>
+			<Post {...post} />
+		</SmallerContainer>
+	</Layout>
 )
 
 export const postQuery = graphql`
-  query($path: String!) {
-    post: markdownRemark(frontmatter: { path: { eq: $path } }) {
-      html
-      timeToRead
-      frontmatter {
-        date(formatString: "MMMM DD, YYYY")
-        edited(formatString: "MMMM DD, YYYY")
-        path
-        title
-        next
-        id
-        thumbnail {
-          childImageSharp {
-            ...imageFields
-          }
-        }
-      }
-    }
-  }
+	query($path: String!) {
+		post: markdownRemark(frontmatter: { path: { eq: $path } }) {
+			html
+			timeToRead
+			frontmatter {
+				date(formatString: "MMMM DD, YYYY")
+				edited(formatString: "MMMM DD, YYYY")
+				path
+				title
+				next
+				id
+				thumbnail {
+					childImageSharp {
+						...imageFields
+					}
+				}
+			}
+		}
+	}
 `
 
 export default Template
