fcba58ae60c07745e772ea5beb1e4f0e2626075b
smakosh <ismai23l@hotmail.com>
2019-01-20 02:11:27 +0100

    store theme chosen to localStorage

--- src/components/common/Provider/index.jsx
+++ src/components/common/Provider/index.jsx
@@ -5,8 +5,10 @@ export const Provider = ({ children }) => {
 	const [theme, setTheme] = useState('light')
 	const toggleTheme = () => {
 		if (theme === 'light') {
+			localStorage.setItem('theme', 'dark')
 			setTheme('dark')
 		} else {
+			localStorage.setItem('theme', 'light')
 			setTheme('light')
 		}
 	}
