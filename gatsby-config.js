e9ec7128839491da5d256402a3ca80810a068678
smakosh <ismai23l@hotmail.com>
2019-11-19 15:48:46 +0100

    chore: remove netlify plugin and redirects

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,7 +20,6 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components',
-    'gatsby-plugin-netlify',
     'gatsby-plugin-catch-links',
     'gatsby-plugin-sitemap',
     'gatsby-transformer-yaml',
